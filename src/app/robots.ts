733693e14963673b70550d19097f308b44f2e334
gowthamavtech <gowtham.av.tech@gmail.com>
2026-05-01 14:49:44 +0530

    fixed the robots.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://imagepdf.tools/sitemap.xml',
+    sitemap: 'https://www.imagepdf.tools/sitemap.xml',
   };
 }
